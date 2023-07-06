76c82529904c2c5699417230636bcdc84089436e
mo <mohammad.joudy@gmail.com>
2023-07-06 12:32:00 +0200

    analysis of derivative method of tau estimation progressed.

--- kernel_est_funcs.py
+++ kernel_est_funcs.py
@@ -1,3 +1,7 @@
+# this script consists of functions to simulate calcium signals from spike trains,
+#smooth them using savitzky-golay and 4 different outlier removal methods in order to 
+#for a line to scatter plot of signal-derivative of signal.
+
 import numpy as np
 #import scipy as sp
 import matplotlib.pyplot as plt
@@ -46,7 +50,6 @@ def sim_calcium(spikes, tau=100, neuron_id=500):
     return calcium_nsp_noisy
 
 
-
 def smoothed_signals(signal, win_len, do_plots=False):
     smooth_cal = sig.savgol_filter(signal, window_length=win_len, deriv=0, delta=1., polyorder=3)
     smooth_deriv = sig.savgol_filter(signal, window_length=win_len, deriv=1, delta=1., polyorder=3)
@@ -63,21 +66,31 @@ def smoothed_signals(signal, win_len, do_plots=False):
 
     return negetive_cal, negetive_deriv
 
+def scatter_all(signal, win_len):
+    smooth_cal = sig.savgol_filter(signal, window_length=win_len, deriv=0, delta=1., polyorder=3)
+    smooth_deriv = sig.savgol_filter(signal, window_length=win_len, deriv=1, delta=1., polyorder=3)
+
+    fig, ax = plt.subplots()
+    ax.scatter(smooth_cal, smooth_deriv, marker='.', s=5)
+    ax.set_xlabel('Calcium Signal')
+    ax.set_ylabel('Derivative of Calcium Signal')
 
 
-def pure_fit(signal, deriv, plot=False):
+def pure_fit(signal, deriv, do_plot=False):
 
     b_pure_fit, a_pure_fit = np.polyfit(signal, deriv, deg=1)
 
-    if plot==True:
+    if do_plot==True:
         fig, ax = plt.subplots()
         ax.scatter(signal, deriv, marker='.', s=5)
         ax.plot(signal, a_pure_fit+b_pure_fit*signal, color='k')
+        ax.set_xlabel('Calcium Signal')
+        ax.set_ylabel('Derivative of Calcium Signal')
     
     return b_pure_fit
 
 
-def iqr_outlier(signal, deriv, threshold=1.5, plot=False):
+def iqr_outlier(signal, deriv, threshold=1.5, do_plot=False):
     
     x = np.array(signal)
     y = np.array(deriv)
@@ -94,7 +107,7 @@ def iqr_outlier(signal, deriv, threshold=1.5, plot=False):
 
     b_iqr, a_iqr = np.polyfit(x[mask], y[mask], deg=1)
     
-    if plot==True:
+    if do_plot==True:
         i_mask = np.logical_not(mask)
         outlier_x = x[i_mask]
         outlier_y = y[i_mask]
@@ -102,18 +115,20 @@ def iqr_outlier(signal, deriv, threshold=1.5, plot=False):
         ax.scatter(inlier_x, inlier_y, marker='.', s=5)
         ax.plot(inlier_x, a_iqr+b_iqr*inlier_x, color='k')
         ax.scatter(outlier_x, outlier_y, marker='.', s=5, color='red')
+        ax.set_xlabel('Calcium Signal')
+        ax.set_ylabel('Derivative of Calcium Signal')
 
     return b_iqr
 
 
-def ransac_outlier(signal, deriv, plot=False):
+def ransac_outlier(signal, deriv, do_plot=False):
 
     ransac = RANSACRegressor()
     x = np.array(signal).reshape(-1, 1)
     y = np.array(deriv)
     ransac.fit(x, y)
 
-    if plot==True:
+    if do_plot==True:
         mask = ransac.inlier_mask_
         i_mask = np.logical_not(mask)
         line_x = np.linspace(min(x), max(x), 100).reshape(-1, 1)
@@ -123,12 +138,14 @@ def ransac_outlier(signal, deriv, plot=False):
         plt.scatter(x[mask], y[mask], label='Inliers', marker='o', s=5)
         plt.scatter(x[i_mask], y[i_mask], label='Outliers', color='red', marker='o', s=5)
         plt.plot(line_x, line_y, color='green', label='Robust Regression Line')
+        ax.set_xlabel('Calcium Signal')
+        ax.set_ylabel('Derivative of Calcium Signal')
 
     
     return ransac.estimator_.coef_[0]
 
 
-def zscore_outlier(signal, deriv, threshold=2, plot=False):
+def zscore_outlier(signal, deriv, threshold=2, do_plot=False):
 
     x = np.array(signal)
     y = np.array(deriv)
@@ -139,12 +156,14 @@ def zscore_outlier(signal, deriv, threshold=2, plot=False):
 
     b_zscore, a_zscore = np.polyfit(x[mask], y[mask], deg=1)
 
-    if plot==True:
+    if do_plot==True:
         i_mask=np.logical_not(mask)
         fig, ax = plt.subplots()
         plt.scatter(x[mask], y[mask], marker='.', s=5)
         plt.scatter(x[i_mask], y[i_mask], marker='.', s=5,color='red')
         plt.plot(x[mask], a_zscore+b_zscore*x[mask], color='k')
+        ax.set_xlabel('Calcium Signal')
+        ax.set_ylabel('Derivative of Calcium Signal')
 
     return b_zscore
 
