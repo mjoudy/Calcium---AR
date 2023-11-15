dbb874e69cabb7c35d57cd25cf32218c67edc5d3
Mohammad <mohammad.joudy@gmail.com>
2023-11-15 23:28:54 +0100

    concatenation of chuncked data added for nemo

--- nemo-py/pipeline/chunk_data.py
+++ nemo-py/pipeline/chunk_data.py
@@ -1,5 +1,9 @@
+import os
 import numpy as np
 
+data_dir = '/home/fr/fr_fr/fr_mj200/'
+data_file = 'spikes-10e4-ms.npy'
+#data_address = data_dir+data_file
 data_address = 'spikes-10e4-ms.npy'
 data = np.load(data_address)
 
@@ -8,11 +12,18 @@ def divide_to_chunks(array, num_chunks):
     chunks = [array[i:i+chunk_size] for i in range(0, array.shape[0], chunk_size)]
     return chunks
 
-def save_chunks(chunks, prefix):
+def save_chunks(chunks, prefix, save_dir):
     for i, chunk in enumerate(chunks):
-        np.save(f'{prefix}_{i}.npy', chunk)
+        filename = os.path.join(save_dir, f'{prefix}_{i}.npy')
+        np.save(filename, chunk)
 
-data_name = data_address.split('.')[0]
-chunks = divide_to_chunks(data, 10)
-save_chunks(chunks, data_name)
+
+chunks_nums = 10
+chunks = divide_to_chunks(data, chunks_nums)
+
+data_name = data_file.split('.')[0]
+#save_dir = '/work/ws/nemo/fr_mj200-lasso_reg-0/pipeline/'
+save_dir = os.getcwd()
+save_chunks(chunks, data_name, save_dir)
+
 
